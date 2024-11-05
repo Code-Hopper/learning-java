9ae17dc22449fc6be33137ead4fe3b01d18badfc
code-hopper <ameykhondekar01@gmail.com>
2024-11-05 21:13:02 +0530

    day 1 java

---
+++ day1/src/App.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+
+public class App {
+    public static void main(String[] args){
+        // statements| input | output
+        // System.out.println("Hello java Using ln");
+        // System.out.print("1). Hello java Using without ln");
+        // System.out.print("2). Hello java Using without ln");
+        // System.out.printf("");
+        // print in formated structure
+
+        // variables in java
+
+        int a = 21;
+        int b = 22;
+        int c = 23;
+
+        float pi = (float) 3.142;
+        // type casting
+
+        double bigPi = 3.142738;
+
+        boolean option = true;
+
+        char value = 'a';
+
+        String name = "amey";
+        // import java.lang.*(everything);
+
+        // printf("the value of a is %d",a);
+        // concatination | joining 2 or more "string" may be with a variable also
+        System.out.println("the value of a is "+a+" !");
+        System.out.println("the value of b is "+b+" !");
+        System.out.println("the value of c is "+c+" !");
+
+        // System.out.println("the sum of a and b is "+a+b+" !");
+        System.out.println("the sum of a and b is "+(a+b)+" !");
+        
+        System.out.println("my name is " +name);
+
+
+    }
+}
