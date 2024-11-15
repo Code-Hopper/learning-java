c46a3160bea47a6ac2cd462cd28e355134c272f7
code-hopper <ameykhondekar01@gmail.com>
2024-11-15 11:18:10 +0530

    oprators

--- day1/src/App.java
+++ day1/src/App.java
@@ -1,7 +1,7 @@
 import java.lang.*;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // statements| input | output
         // System.out.println("Hello java Using ln");
         // System.out.print("1). Hello java Using without ln");
@@ -16,7 +16,7 @@ public class App {
         int c = 23;
 
         float pi = (float) 3.142;
-        // type casting
+        // type casting[]
 
         double bigPi = 3.142738;
 
@@ -29,15 +29,52 @@ public class App {
 
         // printf("the value of a is %d",a);
         // concatination | joining 2 or more "string" may be with a variable also
-        System.out.println("the value of a is "+a+" !");
-        System.out.println("the value of b is "+b+" !");
-        System.out.println("the value of c is "+c+" !");
+        System.out.println("the value of a is " + a + " !");
+        System.out.println("the value of b is " + b + " !");
+        System.out.println("the value of c is " + c + " !");
 
         // System.out.println("the sum of a and b is "+a+b+" !");
-        System.out.println("the sum of a and b is "+(a+b)+" !");
-        
-        System.out.println("my name is " +name);
+        System.out.println("the sum of a and b is " + (a + b) + " !");
 
+        System.out.println("my name is " + name);
+
+        // oprators
+        // airthmatic(+ - * / % ), logical (&&, ||), conditional ( < > <= >= != == ===)
+        // , increement (++value, value++), decreement(--value, value--), assignment(=)
+
+        // int data1 = 1;
+        // int data2 = 2;
+
+        // float answer = (float) data1 % data2;
+
+        // System.out.println(answer);
+
+        // increement and decreement
+
+        // int data3 = 1;
+
+        // data3++;
+
+        // // first value is going to be 1
+        // System.out.println("value before increement is : " + data3);
+
+        // // next value is going to be 2
+        // System.out.println("value after increement is : " + data3);
+
+
+        // for(int i = 0 ; i <  5; ++i){
+        //     System.out.println(i);
+    
+        // }
+
+        int x = 1;
+
+        int a1 = ++x;
+        // int b1 = x++;
+
+        System.out.println(a1);
+        // System.out.println(b1);
 
     }
+
 }
